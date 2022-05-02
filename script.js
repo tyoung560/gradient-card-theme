c9ecd243ef739d3339d62c9206bb3bf6a351d41c
tyoung560 <47755919+tyoung560@users.noreply.github.com>
2022-05-02 10:50:04 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+var width = $(".x-menu-style-vertical").width();
+var offset = width + 0;
+if (window.matchMedia('(min-width: 768px)').matches) {
+  $(".app").css({
+    "margin-left": offset + "px"
+  });
+}
