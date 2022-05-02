15f606c753805ab2e4e3d4f3f8923070b1205120
tyoung560 <47755919+tyoung560@users.noreply.github.com>
2022-05-02 11:15:35 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-var width = $(".x-menu-style-vertical").width();
-var offset = width + 0;
+TB.render('component_layout_3', function(data) {
+    var width = $(".x-menu-style-vertical").width();
+    var offset = width + 0;
 
-$(document).ready(function () {
     if (window.matchMedia('(min-width: 768px)').matches) {
         $(".app").css({
             "margin-left": offset + "px"
