a49b2ef6c64f2f8287a4887f12f100730f47266c
tyoung560 <47755919+tyoung560@users.noreply.github.com>
2022-05-02 10:53:35 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 var width = $(".x-menu-style-vertical").width();
 var offset = width + 0;
-if (window.matchMedia('(min-width: 768px)').matches) {
-  $(".app").css({
-    "margin-left": offset + "px"
-  });
-}
+
+$(document).ready(function () {
+    if (window.matchMedia('(min-width: 768px)').matches) {
+        $(".app").css({
+            "margin-left": offset + "px"
+        });
+    }
+});
